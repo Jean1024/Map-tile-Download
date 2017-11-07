76d20b5388250d3ebd48228d313570e2538ecbad
zhilin <1737923789@qq.com>
2017-11-07 09:59:10 +0800

    优化下载

--- index.js
+++ index.js
@@ -2,34 +2,22 @@ const fs = require('fs')
 const http = require('http')
 const path =  require('path')
 const tMapUrl = 'http://t0.tianditu.com/DataServer?T=vec_c&'
+const download = require('download');
 // 最大缩放 
 const level = 5
-const levelStart = 5
+const levelStart = 1
 let xRange = null
 let yRange = null
-// var server = http.createServer(function(req, res){}).listen(50066);
-// console.log("http start");
-
+let arr = []
 // var url = "http://t0.tianditu.com/DataServer?T=vec_c&x=7&y=2&l=3";
 function getImg(i,x,y) {
-  http.get(`${tMapUrl}x=${x}&y=${y}&l=${i}`, function(res){
-    var imgData = "";
-    res.setEncoding("binary"); //一定要设置response的编码为binary否则会下载下来的图片打不开
-    res.on("data", function(chunk){
-        imgData+=chunk;
-    });
-    res.on("end", function(){
 
-        let savePath = path.resolve(__dirname,'imgs/')
+        let savePath = path.resolve(__dirname,'images/')
         checkPath(savePath)
         checkPath(savePath + `/level${i}`)
         checkPath(savePath + `/level${i}` + `/x-${x}/`)
-        console.log(`./imgs/level${i}/x-${x}/y-${y}.png`)
-        fs.writeFileSync(`./imgs/level${i}/x-${x}/y-${y}.png`, imgData, "binary");
-        
-        
-    });
-  });
+        var imgUrl = `./images/level${i}/x-${x}/y-${y}.png`
+        download(`${tMapUrl}x=${x}&y=${y}&l=${i}`).pipe(fs.createWriteStream(imgUrl));
 }
 // 检查文件夹是否存在
 function fsExistsSync(path) { 
@@ -51,7 +39,6 @@ for (let i = levelStart; i <= level; i++) {
   for (let x = 0; x <= xRange ; x++) {
     for (let y = 0; y <= yRange; y++) {
       getImg(i,x,y)
-      // console.log(`${tMapUrl}x=${x}&y=${y}&l=${i}`)
     }
   }
 }
